9142a12b2540dee3da00917baf20c1788229ce19
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-31 09:58:34 +0300

    Created the Post model

--- models/posts_model.py
+++ models/posts_model.py
@@ -1,2 +1,12 @@
 """The Posts model file"""
 from database.db import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class Post(Base):
+    __tablename__ = "posts"
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable==False)
+    content = Column(String, nullable=False)
+    published = Column(Boolean, default=False)
