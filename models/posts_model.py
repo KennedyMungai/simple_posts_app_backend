ccf5734f760ed9fe45447ede92edea1f36916fd7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-31 09:59:25 +0300

    Updated a dumb mistake

--- models/posts_model.py
+++ models/posts_model.py
@@ -8,6 +8,6 @@ class Post(Base):
     __tablename__ = "posts"
     
     id = Column(Integer, primary_key=True, nullable=False)
-    title = Column(String, nullable==False)
+    title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, default=False)
