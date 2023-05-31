8275f94b867582426a04fd7fb89abee61f49be34
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-31 09:43:58 +0300

    Loaded the environment variables

--- database/db.py
+++ database/db.py
@@ -5,3 +5,5 @@ from dotenv import find_dotenv, load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+load_dotenv(find_dotenv())
