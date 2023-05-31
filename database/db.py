942fb5e7e1d0e8d21e1b9a8f6a828c03cc2530a4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-31 09:43:00 +0300

    Added and reorganized some sqlalchemy imports

--- database/db.py
+++ database/db.py
@@ -3,3 +3,5 @@ import os
 
 from dotenv import find_dotenv, load_dotenv
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
